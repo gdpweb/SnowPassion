0baed4148865a0763d3d23112e3244c5659b3135
gdpweb <stephanebriere@gdpweb.fr>
2018-09-26 23:17:01 +0200

    edit (form+ méthods + views)

---
+++ src/AppBundle/Form/TrickEditType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use AppBundle\Repository\GroupeRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickEditType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('description', TextareaType::class, array(
+                'attr' => array(
+                    'rows' => 10
+                )
+            ))
+            ->add('groupe', EntityType::class, array(
+                'class' => 'AppBundle\Entity\Groupe',
+                'choice_label' => 'nom',
+                'multiple' => false,
+                'query_builder' => function (GroupeRepository $repository) {
+                    return $repository->getListGroupes();
+                }
+            ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Trick'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_trick';
+    }
+
+
+}
---
+++ web/assets/js/editTrick.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    $('.delete').click(function () {
+
+        let titre;
+
+        if ($(this).attr('title')===null) {
+            titre = 'SnowPassion';
+        } else {
+            titre = $(this).attr('title');
+        }
+
+        $('.modal-title').html(titre);
+        const url = $(this).data('href');
+        $.ajax({
+            type: 'post',
+            url: url,
+            success: function (data) {
+                $('.modal-body').html(data);
+                $('.modal').modal("show");
+            }
+        });
+    });
+
+});
